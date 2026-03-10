3aa722d80fcf954eeb8e793c5bdf03d6db458dca
steven77390 <steven09071999@gmail.com>
2026-03-11 00:07:58 +0100

    Create App.jsx to import and render VerreProgressif component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerreProgressif from './VerreProgressif';
+
+const App = () => {
+    return (
+        <div>
+            <VerreProgressif />
+        </div>
+    );
+};
+
+export default App;
